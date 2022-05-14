fff693328cf9c7e139c111a5a9d7048b32898ca3
vitoremanueldev <veslima3@gmail.com>
2022-05-14 03:45:56 -0300

    Adicionando valores no enum

--- src/br/edu/ifba/inf011/aval1/equipamento/EquipamentoEnum.java
+++ src/br/edu/ifba/inf011/aval1/equipamento/EquipamentoEnum.java
@@ -1,9 +1,15 @@
 package br.edu.ifba.inf011.aval1.equipamento;
 
 public enum EquipamentoEnum {
-	
-	MAQUINAS,
-	HALTERES,
-	ACESSORIOS
+
+	MAQUINAS("br.edu.ifba.inf011.aval1.equipamento.Maquinas"),
+	HALTERES("br.edu.ifba.inf011.aval1.equipamento.Halteres"),
+	ACESSORIOS("br.edu.ifba.inf011.aval1.equipamento.Acessorios");
+
+	public final String classe;
+
+	EquipamentoEnum(String classe) {
+		this.classe = classe;
+	}
 
 }
